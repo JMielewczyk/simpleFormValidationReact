db1423781db0e60c998d71622225cb27772c6e12
Jakub <jkbmielewczyk@gmail.com>
2022-11-06 12:23:39 +0100

    Updates

---
+++ precache-manifest.461f09dfbbeb10c73a342650831f3ca4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6842a8b1abd44a4706f0",
+    "url": "/simpleFormValidationReact/static/css/main.2e979f55.chunk.css"
+  },
+  {
+    "revision": "6842a8b1abd44a4706f0",
+    "url": "/simpleFormValidationReact/static/js/main.c8503fbb.chunk.js"
+  },
+  {
+    "revision": "06d70bd0da420c2f2d22",
+    "url": "/simpleFormValidationReact/static/js/runtime~main.2c6ab3e3.js"
+  },
+  {
+    "revision": "be86067d0035c1a45e44",
+    "url": "/simpleFormValidationReact/static/js/2.5c50cce9.chunk.js"
+  },
+  {
+    "revision": "661362becb59cd80afdf2095181865f5",
+    "url": "/simpleFormValidationReact/index.html"
+  }
+];
